9eb433c03bf445f69b77f050d311955f391aee51
George Moschovitis <george.moschovitis@gmail.com>
2014-08-02 22:25:11 +0300

    Introduced new-post command to flare cli.

--- bin/flare.dart
+++ bin/flare.dart
@@ -1,13 +1,27 @@
 #!/usr/bin/env dart
 
+// TODO: Use the logger package.
+
+import 'dart:io';
+
+import 'package:intl/intl.dart';
 import 'package:args/args.dart';
 
 /// The 'epoch' for the id sequence is 1 Jan 2000.
 final int ID_EPOCH_MS = new DateTime(2000).millisecondsSinceEpoch;
 
+final DateFormat DATE_FORMAT = new DateFormat('yyyy/MM/dd');
+
+/// # Examples
+///
+/// $ flare generate-post-id
+/// $ flare new-post "This is my new post"
+/// $ flare new-post --date=2014/08/02 "This is another post"
 void main(List<String> args) {
-  final parser = new ArgParser(allowTrailingOptions: true);
+  final parser = new ArgParser();
   parser.addCommand('generate-post-id');
+  final newPost = parser.addCommand('new-post');
+  newPost.addOption('date', abbr: 'd', help: "The creation data of the post");
 
   final cli = parser.parse(args);
 
@@ -15,6 +29,16 @@ void main(List<String> args) {
     switch (cli.command.name) {
       case 'generate-post-id':
         print(generateNewPostId());
+        break;
+
+      case 'new-post':
+        var date;
+        if (cli.command.wasParsed('date')) {
+          date = cli.command['date'];
+        } else {
+          date = DATE_FORMAT.format(new DateTime.now());
+        }
+        createNewPost(cli.command.rest.first, date: date);
         break;
     }
   } else {
@@ -26,4 +50,37 @@ void main(List<String> args) {
 /// and is independent of post metadata (e.g. title).
 String generateNewPostId() {
   return ((new DateTime.now().millisecondsSinceEpoch - ID_EPOCH_MS)/ 1000).toStringAsFixed(0);
+}
+
+/// Creates subdirectories and the file for a new post.
+void createNewPost(String title, {DateTime date}) {
+  final dirname = 'web/posts/${date}';
+  final filename = '${_slugify(title)}.md';
+
+  new Directory(dirname).create(recursive: true);
+
+  final file = new File('$dirname/$filename');
+
+  // TODO: handle date formatting.
+
+  if (file.existsSync()) {
+    print("Can't create post, '$dirname/$filename' already exists!");
+  } else {
+    file.writeAsStringSync(
+'''<!--
+title: $title
+date: $date
+id: ${generateNewPostId()}
+labels:
+  - Label
+-->
+TODO: Add the text of your post here!'''
+    );
+    print("Created '$dirname/$filename'");
+  }
+}
+
+String _slugify(String title) {
+  return title.replaceAll(new RegExp(' '), '-').
+      replaceAll(new RegExp(r"['?!.,]"), '').toLowerCase();
 }
